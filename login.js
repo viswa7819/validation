dfcc4caad63d66d2285a9bb05893994912439122
viswa7819 <157481940+viswa7819@users.noreply.github.com>
2024-02-02 15:47:23 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+// function validateForm() {
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+//     if (username.trim() === '') {
+//         alert('Please enter your username or email.');
+//         return false;
+//     }
+
+//     if (password.trim() === '') {
+//         alert('Please enter your password.');
+//         return false;
+//     }
+
+//     alert('Form submitted successfully!');
+//     return true;
+// }
